ad835cd799bbac6645640650ab2c5e75278014f6
Ramu V <Ramu.Venkatesan@infoservices.com>
2025-07-29 14:28:55 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddSingleton<EmployeesPortal.Service.EmployeeServices>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -8,7 +7,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
